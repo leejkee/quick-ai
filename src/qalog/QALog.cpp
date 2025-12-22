4a1024f6cbb8eb73ee00f2529da6d6d375e4350c
leejkee <leejykee@yeah.net>
2025-12-23 01:02:54 +0800

    refactor(QALog): modernize LogManager and remove QObject dependency

--- src/qalog/QALog.cpp
+++ src/qalog/QALog.cpp
@@ -2,6 +2,11 @@
 // Created by 31305 on 2025/11/25.
 //
 #include <QALog.h>
+#include <QDateTime>
+#include <QDir>
+#include <QFileInfo>
+#include <filesystem>
+#include <iostream>
 
 namespace QA::Log
 {
@@ -12,32 +17,182 @@ LogManager& LogManager::instance()
     return instance;
 }
 
-void LogManager::log(const LogLevel level, const QString& msg)
+void LogManager::initLogger(const LogMode mode, const QString& logBaseName,
+                      const int bufferSize)
 {
-    switch (level)
+    QMutexLocker locker(&m_mutex);
+    m_mode = mode;
+    m_maxBufferSize = bufferSize;
+    const QFileInfo fileInfo(logBaseName);
+    m_baseDir = fileInfo.absolutePath();
+    m_baseName = fileInfo.baseName();
+    if (const QDir dir(m_baseDir); !dir.exists())
     {
-    case LogLevel::Debug:
+        if (!dir.mkpath("."))
         {
-            qDebug() << "[DEBUG]" << msg;
-            return;
-        }
-    case LogLevel::Info:
-        {
-            qInfo() << "[INFO ]" << msg;
-            break;
-        }
-    case LogLevel::Error:
-        {
-            qCritical() << "[ERROR]" << msg;
-            break;
-        }
-    case LogLevel::Warn:
-        {
-            qWarning() << "[WARN ]" << msg;
+            std::cerr << "LogManager: Failed to create log directory: "
+                  << m_baseDir.toStdString() << std::endl;
         }
     }
-    Q_EMIT signalLogMessage(level, msg);
+
+    if (m_mode == LogMode::WriteAll)
+    {
+        const QString logFileName = generateLogFileName("");
+        m_logFile.setFileName(logFileName);
+        if (!m_logFile.open(QIODevice::WriteOnly | QIODevice::Text))
+        {
+            std::cerr << "LogManager: Failed to create log file: "
+                      << logFileName.toStdString() << std::endl;
+        }
+        else
+        {
+            m_outStream.setDevice(&m_logFile);
+            m_outStream << "=== LOG STARTED AT "
+                        << QDateTime::currentDateTime().toString(
+                                   "yyyy-MM-dd HH:mm:ss")
+                        << " ===\n";
+        }
+    }
+    else if (m_mode == LogMode::WriteOnFail)
+    {
+        m_logBuffer.resize(m_maxBufferSize);
+        m_bufferIndex = 0;
+        m_bufferFull = false;
+    }
+    qInstallMessageHandler(LogManager::messageHandler);
 }
 
+LogManager::~LogManager()
+{
+    qInstallMessageHandler(nullptr);
+    if (m_logFile.isOpen())
+    {
+        m_logFile.close();
+    }
+}
+
+QString LogManager::generateLogFileName(const QString& tag) const
+{
+    QString timestamp =
+            QDateTime::currentDateTime().toString("yyyyMMdd-HHmmss");
+    return QString("%1/%2%3_%4.log")
+            .arg(m_baseDir, m_baseName, tag, timestamp);
+}
+
+void LogManager::messageHandler(const QtMsgType type,
+                                const QMessageLogContext& context,
+                                const QString& msg)
+{
+    instance().handleLogMessage(type, context, msg);
+}
+
+void LogManager::handleLogMessage(const QtMsgType type,
+                                  const QMessageLogContext& context,
+                                  const QString& msg)
+{
+    QMutexLocker locker(&m_mutex);
+
+    QString levelStr;
+    switch (type)
+    {
+    case QtDebugMsg:
+        levelStr = "[DEBUG]";
+        break;
+    case QtInfoMsg:
+        levelStr = "[INFO ]";
+        break;
+    case QtWarningMsg:
+        levelStr = "[WARN ]";
+        break;
+    case QtCriticalMsg:
+        levelStr = "[ERROR]";
+        break;
+    case QtFatalMsg:
+        levelStr = "[FATAL]";
+        break;
+    }
+
+    QString fileName = "unknown";
+    if (context.file)
+    {
+        fileName = QString::fromStdString(
+                std::filesystem::path(context.file).filename().string());
+    }
+
+    QString timeStr =
+            QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss.zzz");
+    const QString formattedMessage = QString("%1 %2 [%3:%4] %5")
+                                       .arg(timeStr, levelStr, fileName)
+                                       .arg(context.line)
+                                       .arg(msg);
+
+    fprintf(type >= QtCriticalMsg ? stderr : stdout,
+            "%s\n",
+            qPrintable(formattedMessage));
+    fflush(type >= QtCriticalMsg ? stderr : stdout);
+
+    if (m_mode == LogMode::ConsoleOnly)
+    {
+        return;
+    }
+
+    if (m_mode == LogMode::WriteAll)
+    {
+        if (m_logFile.isOpen())
+        {
+            m_outStream << formattedMessage << "\n";
+            m_outStream.flush();
+        }
+    }
+    else if (m_mode == LogMode::WriteOnFail)
+    {
+        m_logBuffer[m_bufferIndex] = formattedMessage;
+        m_bufferIndex++;
+        if (m_bufferIndex >= m_maxBufferSize)
+        {
+            m_bufferIndex = 0;
+            m_bufferFull = true;
+        }
+
+        if (type == QtCriticalMsg || type == QtFatalMsg)
+        {
+            dumpBufferToNewFile();
+        }
+    }
+}
+
+void LogManager::dumpBufferToNewFile()
+{
+    const QString crashFileName = generateLogFileName("_crash");
+
+    QFile crashFile(crashFileName);
+    if (!crashFile.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        std::cerr << "LogManager: Failed to create crash dump file: "
+                  << crashFileName.toStdString() << std::endl;
+        return;
+    }
+
+    QTextStream stream(&crashFile);
+    stream << "=== CRASH DUMP TRIGGERED ===\n";
+
+    const int start = m_bufferFull ? m_bufferIndex : 0;
+    const int count = m_bufferFull ? m_maxBufferSize : m_bufferIndex;
+
+    for (int i = 0; i < count; ++i)
+    {
+        const int idx = (start + i) % m_maxBufferSize;
+        stream << m_logBuffer[idx] << "\n";
+    }
+
+    stream << "=== END OF DUMP ===\n";
+    crashFile.close();
+
+    std::cerr << "Crash log saved to: " << crashFileName.toStdString()
+              << std::endl;
+
+    m_bufferIndex = 0;
+    m_bufferFull = false;
+}
 
 } // namespace QA::Log
--- src/qalog/include/QALog.h
+++ src/qalog/include/QALog.h
@@ -2,37 +2,82 @@
 // Created by 31305 on 2025/11/25.
 //
 #pragma once
-
-#include <QDir>
+#include <QFile>
+#include <QMessageLogContext>
 #include <QMutex>
+#include <QString>
+#include <QTextStream>
+#include <QVector>
+
 namespace QA::Log
 {
-enum class LogLevel
+
+enum class LogMode
 {
-    Debug,
-    Info,
-    Warn,
-    Error
+    ConsoleOnly, // print log to console only
+    WriteAll,    // write log to logs/app_xxxxxxxx-120000.log
+    WriteOnFail  // write log generated when the program crashes to
+                 // logs/app_crash_xxxxxx-120000.log
 };
 
-class LogManager final : public QObject
+class LogManager
 {
-    Q_OBJECT
 public:
     static LogManager& instance();
 
-    void log(LogLevel level, const QString& msg);
-
-Q_SIGNALS:
-    void signalLogMessage(LogLevel level, const QString& msg);
+    /**
+     * @brief init log manager
+     * @param mode log mode
+     * @param logBaseName the path of log file (e.g. "log/app")
+     * @param bufferSize Number of recent log entries to retain in memory for
+     *                   WriteOnFail mode. Upon crash, this buffer is flushed
+     *                   to disk to aid debugging.
+     */
+    void initLogger(LogMode mode = LogMode::ConsoleOnly,
+              const QString& logBaseName = "log/app",
+              int bufferSize = 10);
 
 private:
     LogManager() = default;
+
+    ~LogManager();
+
     Q_DISABLE_COPY(LogManager)
+
+    static void messageHandler(QtMsgType type,
+                               const QMessageLogContext& context,
+                               const QString& msg);
+
+    void handleLogMessage(QtMsgType type,
+                          const QMessageLogContext& context,
+                          const QString& msg);
+
+    [[nodiscard]] QString generateLogFileName(const QString& tag) const;
+
+    void dumpBufferToNewFile();
+
+    LogMode m_mode = LogMode::ConsoleOnly;
+    int m_maxBufferSize = 10;
+
+    QString m_baseDir;  // e.g., "logs/"
+    QString m_baseName; // e.g., "app"
+
+    QFile m_logFile;
+    QTextStream m_outStream;
+    QMutex m_mutex;
+
+    QVector<QString> m_logBuffer;
+    int m_bufferIndex = 0;
+    bool m_bufferFull = false;
 };
 
-#define QA_LOG_INFO(msg) Log::LogManager::instance().log(Log::LogLevel::Info, msg)
-#define QA_LOG_WARN(msg) Log::LogManager::instance().log(Log::LogLevel::Warn, msg)
-#define QA_LOG_ERR(msg) Log::LogManager::instance().log(Log::LogLevel::Error, msg)
+#define QA_LOG_INFO(msg)                                                       \
+    QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO).info() << msg
+#define QA_LOG_WARN(msg)                                                       \
+    QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO).warning() << msg
+#define QA_LOG_ERR(msg)                                                        \
+    QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO).critical() << msg
+#define QA_LOG_DEBUG(msg)                                                      \
+    QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO).debug() << msg
 
 } // namespace QA::Log
